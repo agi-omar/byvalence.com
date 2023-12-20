6b4229f4853595077dff372ea7795644cd140777
omaro <153914252+agi-omar@users.noreply.github.com>
2023-12-20 23:34:47 +0300

    deleted custom font-size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,15 +16,7 @@ module.exports = {
         serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.serif],
       },
-      fontSize: {
-        sm: '0.8rem',
-        base: '1rem',
-        xl: '1.25rem',
-        '2xl': '1.563rem',
-        '3xl': '1.953rem',
-        '4xl': '2.441rem',
-        '5xl': '3.052rem',
-      }
+
     },
   },
   plugins: [require('@tailwindcss/typography')],
